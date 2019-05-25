b0e3d1131465da6f9465d7e5aa44a28c75b45241
eray01 <ylmz.ery@gmail.com>
2019-05-25 20:46:53 +0300

    change again

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/nirayhospital'));
+app.use(express.static(__dirname + '/dist/hospitalReport'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/nirayhospital/index.html'));
+res.sendFile(path.join(__dirname+'/dist/hospitalReport/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
