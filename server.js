83505b9a324137615929617d51e324e4b7261760
eray01 <ylmz.ery@gmail.com>
2019-05-25 20:41:32 +0300

    change app name

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/nirayhospital'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/nirayhospital/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
